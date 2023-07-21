73f9752d2afe11d3cf2ffeb192a652f65e3a1205
hyunju <wjdguswn0954@gmail.com>
2023-07-21 23:57:00 +0900

    2022-1 스위프트 10주차 과제

---
+++ 3-1/swift programing/person.swift
@@ -0,0 +1,13 @@
+// '사람'을 나타내기 위한 클래스를 정의하시오.
+// 사람의 나이, 성별을 나타내는 프라퍼티와 '생각하다'라는 메소드를 추가하시오.
+
+import Foundation
+
+class Person {
+    var age: Int
+    var sex: String
+
+    func think() {
+
+    }
+}
