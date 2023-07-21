da70e5177c735340d70d241dd6fd86e065d47b9b
hyunju <wjdguswn0954@gmail.com>
2023-07-21 23:55:10 +0900

    자바 2주차 과제

--- 3-1/java1/ex02.java
+++ 3-1/java1/ex02.java
@@ -1,41 +1,27 @@
 import java.util.Scanner;
 
-
-
 public class ex02
-
 {
-
     public static void main( String[] args )
 
     {
-
         Scanner input = new Scanner( System.in );
 
         int number1;
-
         int number2;
-
-
 
         System.out.print( "Enter first integer: " );
 
         number1 = input.nextInt();
 
-
-
         System.out.print( "Enter second integer: " );
 
         number2 = input.nextInt();
 
-
-
         int sum = number1 + number2;
 
         System.out.printf( "Sum is %o in octal, ", sum);
-
         System.out.printf( "%d in decimal, ", sum);
-
         System.out.printf( "%x in hexadecimal.", sum);
 
     }
